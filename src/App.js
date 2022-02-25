c07f95d7c1fedb0a37cede3708052af4afbc7867
Edward Suwirya <edward_suwirya@yahoo.com>
2022-02-25 10:09:42 +0700

    ToDoScreen implementation

--- src/App.js
+++ src/App.js
@@ -7,15 +7,12 @@
  */
 
 import React from 'react';
-import {SafeAreaView, ScrollView, StyleSheet,} from 'react-native';
-import Heading from "./components/Heading";
-import Input from "./components/Input";
-import SubmitButton from "./components/SubmitButton";
-import ToDoList from "./containers/ToDoList";
-import TabBar from "./containers/TabBar";
+import {SafeAreaView, StyleSheet,} from 'react-native';
+import ToDoScreen from "./screens/ToDoScreen";
 
 /*
 Component Creation Step
+0. ToDoScreen
 1. Buat Heading
 2. Buat Input
 3. Buat SubmitButton
@@ -25,58 +22,15 @@ Component Creation Step
  */
 
 const App = () => {
-    let inputValue = "";
-    let todos = [{
-        todoIndex: 1,
-        title: 'Title 1',
-        isComplete: false
-    }, {
-        todoIndex: 2,
-        title: 'Title 2',
-        isComplete: false
-    }];
-
-    let type = '';
-    const inputChange = (inputValue) => {
-        console.log('Input Value: ', inputValue);
-    }
-
-    const submitTodo = () => {
-        console.log('Submit');
-    }
-
-    const toggleComplete = (todoIndex) => {
-        console.log('Complete : ', todoIndex);
-    }
-    const deleteTodo = (todoIndex) => {
-        console.log('Delete : ', todoIndex);
-    }
-
-    const setType = (type) => {
-        console.log('Set Type : ' + type);
-    }
     return (
         <SafeAreaView style={styles.container}>
-            <ScrollView
-                contentInsetAdjustmentBehavior="automatic" keyboardShouldPersistTaps="never" style={styles.content}>
-                <Heading/>
-                <Input inputValue={inputValue} inputChange={(text) => inputChange(text)}/>
-                <ToDoList todos={todos} toggleComplete={toggleComplete}
-                          deleteTodo={deleteTodo} type={type}/>
-                <SubmitButton submitTodo={submitTodo}/>
-            </ScrollView>
-            <TabBar type={type} setType={setType}/>
+            <ToDoScreen/>
         </SafeAreaView>
     );
 };
-
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-    },
-    content: {
-        flex: 1,
-        paddingTop: 60
+        flex: 1
     }
 });
 
