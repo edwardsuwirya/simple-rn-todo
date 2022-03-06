a886d5d7aed2043d9fcb644fd3a2083787f7a0b7
Edward Suwirya <edward_suwirya@yahoo.com>
2022-03-06 15:30:50 +0700

    Navigation Helper Test

---
+++ __tests__/navigation/NavigationHelper-test.js
@@ -0,0 +1,38 @@
+import {LOGIN_PATH} from "../../src/navigation/NavigationPath";
+
+describe('Navigation Helper', () => {
+    jest.mock('../../src/navigation/RootNavigation', () => (
+        {
+            navigate: jest.fn()
+        }
+    ));
+    const importModule = (test) => {
+        return Promise.all([
+            import('../../src/navigation/RootNavigation'),
+            import('../../src/navigation/NavigationHelper')])
+            .then(([moduleRootNavigation, moduleNavigationHelper]) => {
+                test(moduleRootNavigation, moduleNavigationHelper);
+            });
+    }
+    it('navigate to Login', async () => {
+        return importModule((moduleRootNavigation, moduleNavigationHelper) => {
+            const nav = jest.spyOn(moduleRootNavigation, "navigate");
+            moduleNavigationHelper.goToLogin();
+            expect(nav).toHaveBeenCalledWith(LOGIN_PATH, null, true)
+        })
+    });
+    it('navigate to screen', async () => {
+        return importModule((moduleRootNavigation, moduleNavigationHelper) => {
+            const nav = jest.spyOn(moduleRootNavigation, "navigate");
+            moduleNavigationHelper.goToScreen('path', false);
+            expect(nav).toHaveBeenCalledWith('path', null, false)
+        })
+    });
+    it('navigate to screen with params', async () => {
+        return importModule((moduleRootNavigation, moduleNavigationHelper) => {
+            const nav = jest.spyOn(moduleRootNavigation, "navigate");
+            moduleNavigationHelper.goToScreenWithParams('path', {}, false);
+            expect(nav).toHaveBeenCalledWith('path', {}, false)
+        })
+    });
+})
